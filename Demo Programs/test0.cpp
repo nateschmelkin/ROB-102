a42646790b295c11b712c891d950f7e81663d9ec
Nate Schmelkin <nate@schmelkin.com>
2025-09-08 12:50:54 -0400

    Finished p0

--- Demo Programs/test0.cpp
+++ Demo Programs/test0.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() { //delimiter
-    char a = 65;
-    cout << a;
+    
+    string inputDist;
+    int dist;
+    cout << "Enter Distance: ";
+    getline(cin, inputDist);
+    dist = stoi(inputDist);
+
+    if (dist > 50) {
+        cout << "Path Clear";
+    } else if (dist <= 50 && dist >= 20) {
+        cout << "Thing is close slow down";
+    } else {
+        cout << "IDK WHAT THIS IS";
+    }
+
 }
